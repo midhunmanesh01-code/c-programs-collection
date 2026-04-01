033198e0c267145fd9001199c766f4fd5a8da086
Midhun Manesh <midhunmanesh01@gmail.com>
2026-04-01 18:06:41 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Functions/areasq.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int area()
+{
+    float s;
+    printf("Enter the side of square: ");
+    scanf("%f",&s);
+    return s*s;
+}
+int main()
+{
+    float result;
+    result=area();
+    printf("Area of square=%f",result);
+    return 0;
+}
