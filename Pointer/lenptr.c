d8f7f34ccea7bf179cd10d3813c93bb381ac651d
Midhun Manesh <midhunmanesh01@gmail.com>
2026-04-11 20:32:05 +0530

    Added a program to calculate the length of a string using pointers in C

---
+++ Pointer/lenptr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+    char str[20];
+    int i=0;
+    char *ptr=str;
+    printf("Enter a string: ");
+    scanf("%[^\n]",str);
+    while(*ptr!='\0') {
+        i++;
+        ptr++;
+    }
+    printf("Length of the string is %d\n",i);
+}
